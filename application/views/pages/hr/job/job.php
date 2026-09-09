52bd1588657388ff9b8644793db769388ce613c0
John Lee <johnlhy25@gmail.com>
2026-09-09 13:18:18 +0800

    Corrected Metadata

--- application/views/pages/hr/job/job.php
+++ application/views/pages/hr/job/job.php
@@ -14,7 +14,7 @@
     <!-- Favicon & Touch Icons -->
     <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url()?>assets/img/favicon-16x16.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url()?>assets/img/favicon-32x32.png">
-    <link rel="icon" type="image/x-icon" href="<?= base_url()?>favicon.ico">
+    <link rel="icon" type="image/x-icon" href="<?= base_url()?>favicon.png">
     <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url()?>assets/img/apple-touch-icon.png">
     <link rel="manifest" href="/site.webmanifest">
 
